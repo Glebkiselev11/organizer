c5988d95d3798a543aebdf33d96cafb52f73a9cb
glebkiselev11 <12bny23gleb@gmail.com>
2020-03-15 22:18:29 +0300

    Добавил логику для работы с органайзером

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DateService } from "../shared/date.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-organizer',
-  templateUrl: './organizer.component.html',
-  styleUrls: ['./organizer.component.scss']
+  selector: "app-organizer",
+  templateUrl: "./organizer.component.html",
+  styleUrls: ["./organizer.component.scss"]
 })
 export class OrganizerComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(public dateService: DateService) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl("", Validators.required)
+    });
   }
 
+  submit() {
+    // Вытаскиваем из формы значение
+    const { title } = this.form.value;
+    console.log(title);
+  }
 }
